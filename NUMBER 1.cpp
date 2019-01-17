35e9def6e9786b79fcc905e0c91ba34c53fa3216
Fishball pree <46342390+jd666666666666666@users.noreply.github.com>
2019-01-17 17:38:12 +0800

    Add files via upload

---
+++ NUMBER 1.cpp
@@ -0,0 +1,10 @@
+#include <cstring>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x =25;
+	int *ptr= &x;
+	
+	cout<<*ptr <<endl;
+}
---
+++ NUMBER 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	char name[]= "JeeDee";
+	char *ptr = name;
+	
+		cout<<ptr<<endl;
+}
+
